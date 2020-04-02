8cfd0db9fc10367a73447936e4e04648367f9110
unknown <bamdal0615@gmail.com>
2020-04-02 23:37:37 +0900

    101

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pandas as pd
-df = pd.read_csv('./student.csv')
+from collections import OrderedDict
 
+flist =  [
+    {'name': 'John', 'age': 25, 'job': 'student'},
+    {'name': 'Sam', 'age': 35, 'job': 'staff'}
+]
 
-print(df)
+df1 = pd.DataFrame(flist)
+df1 = df1[['name', 'job', 'age']]
+# df = pd.read_csv('./student.csv')
+print(df1.head())
+
+Olist = OrderedDict(
+    [
+        ('name', ['John', 'Nate']),
+        ('age', [25, 30]),
+        ('job', ['student', 'staff'])
+
+    ]
+)
+
+df2 = pd.DataFrame.from_dict(Olist)
+
+print(df2)
