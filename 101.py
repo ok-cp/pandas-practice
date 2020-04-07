b71c1b23fdd9f0094ef07168d8de05db1e9db7d9
unknown <bamdal0615@gmail.com>
2020-04-07 23:58:02 +0900

    concat

--- 101.py
+++ 101.py
@@ -73,3 +73,33 @@ marathon_2017['Official Time New Sec'] = marathon_2017['Official Time Sec'].asty
 print(marathon_2017.head())
 
 ####################
+
+
+# Load the CSV files "marathon_results_2015 ~ 2017.csv" under "data" folder
+marathon_2015 = pd.read_csv("./data/marathon_results_2015.csv")
+marathon_2016 = pd.read_csv("./data/marathon_results_2016.csv")
+marathon_2017 = pd.read_csv("./data/marathon_results_2017.csv")
+
+# Merge 2015, 2016 and 2017 files into marathon_2015_2017 file index by Official Time
+marathon_2015_2017 = pd.concat([marathon_2015, marathon_2016, marathon_2017], ignore_index=True, sort=False).set_index('Official Time')
+
+
+# Display data frame statistics using the .describe() method
+print(marathon_2015_2017.describe())
+
+# Sort data by Age 
+marathon_2015_2017.__________(by=[___________])
+
+# Display the first five initial rows using the .head() method
+print(marathon_2015_2017._______)
+
+# Sort data Descending order by Age 
+marathon_2015_2017.__________(by=__________, ascending=______)
+
+# Display the first five initial rows using the .head() method
+print(marathon_2015_2017._______)
+
+# Save to CSV file "marathon_2015_2017.csv" with header
+marathon_2015_2017._________("./data/marathon_2015_2017.csv", index = None, header=_____)
+
+######
